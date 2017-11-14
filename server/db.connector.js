cbb6a24e8db9a444d10dc5bee2762eb89ee73f47
manuelrocha <manuelalexandrepinhorocha@gmail.com>
2017-11-14 21:49:57 +0000

    Add files via upload

---
+++ server/db.connector.js
@@ -0,0 +1,50 @@
+let mysql = require('mysql');
+
+module.exports = {
+	
+	connect (host, user, password) {
+    	return new Promise((resolve, reject) => {
+			this.connection = mysql.createConnection({
+				host: host,
+				user: user,
+				password: password
+			});
+
+			this.connection.connect((err) => {
+				if (err) {
+					reject();
+					throw err;
+				} else {
+					resolve();
+					console.log('Connected to database ' + user + '@' + host);
+				}
+			});
+		});
+	},
+	
+	query (query) {		
+		console.log('Query: ' + query);
+		return new Promise((resolve, reject) => {
+			this.connection.query(query, (err, result) => {
+				if (err) {
+					reject
+					throw err
+				} else  {
+					resolve(result);
+				}
+			});
+		});
+	},
+	
+	insert (table, values) {
+		let query = 'INSERT INTO ' + TABLE + ' values(';
+
+		values.each((value) => {
+			query = query + value + ',';
+		});
+
+		query = query + ')';
+
+		return 	this.query(query);
+	}
+};
